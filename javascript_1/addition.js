82c65b26f07a69ac38cd8d29bcc5ae37c8eaf610
w1004me <86635205+w1004me@users.noreply.github.com>
2022-05-16 02:11:23 +0900

    Add files via upload

---
+++ javascript_1/addition.js
@@ -0,0 +1,6 @@
+﻿    var num1 = prompt("첫번째 수: ");
+	var num2 = prompt("두번째 수: ");
+	var sum = parseInt(num1) + parseInt(num2);
+	
+    document.write(num1+"과 "+num2+"를 더하면 <br>");
+    document.write(sum+ "입니다.");
---
+++ javascript_1/today.js
@@ -0,0 +1,4 @@
+﻿    var today=new Date();
+    var display = today.toLocaleString();
+    document.write("<h1>오늘은 며칠인가요?</h1>");
+    document.write(display+ "입니다.");
